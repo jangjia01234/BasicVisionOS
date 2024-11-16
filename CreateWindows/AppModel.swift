d0611b8666babb1b69d9e8a97511430940bc85a3
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-11-16 23:36:11 +0900

    Initial Commit

---
+++ CreateWindows/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  CreateWindows
+//
+//  Created by Jia Jang on 11/16/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ CreateWindows/CreateWindowsApp.swift
@@ -0,0 +1,33 @@
+//
+//  CreateWindowsApp.swift
+//  CreateWindows
+//
+//  Created by Jia Jang on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateWindowsApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
