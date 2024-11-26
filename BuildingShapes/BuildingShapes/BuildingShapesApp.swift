700d088ada339f65188e0bf838c963b0642f32f5
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-11-26 18:16:55 +0900

    :adhesive_bandage: Simple fix

---
+++ BuildingShapes/BuildingShapes/BuildingShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingShapesApp.swift
+//  BuildingShapes
+//
+//  Created by Jia Jang on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BuildingShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildingShapes/BuildingShapes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BuildingShapes
+//
+//  Created by Jia Jang on 11/26/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Building Shapes")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
