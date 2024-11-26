6fced11541ca5f583184d68c05bcc689ebdd3c4d
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-11-26 19:48:56 +0900

    :adhesive_bandage: First commit

---
+++ WorkingWithWindows/WorkingWithWindows/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WorkingWithWindows
+//
+//  Created by Jia Jang on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Working With Windows")
+        }
+    }
+}
+
+//#Preview(windowStyle: .automatic) {
+//    ContentView()
+//}
---
+++ WorkingWithWindows/WorkingWithWindows/WorkingWithWindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithWindowsApp.swift
+//  WorkingWithWindows
+//
+//  Created by Jia Jang on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithWindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
